f5e04b9abe4c0eb4a51bc791ca916b74660e4797
Youngjin <youngjin033@gmail.com>
2021-04-29 17:55:05 +0900

    멤버 엔티티 생성 및 관련 jpa 로직 작성

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,27 +4,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        //code
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
+        try{
+            //Member findMember = em.find(Member.class, 1L);
+            //실제론 스프링이 나머지 코드를 다 실행해주어 persist만 하면됨
+            //엔티티 대상으로 쿼리를 날리게 코드 작성(DB, 방언에 종속적이지 않음)
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
-        member.setId(1L);
-        member.setName("HelloA");
-
-        em.persist(member);
-
-        tx.commit();
-
-        em.close();
-
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
