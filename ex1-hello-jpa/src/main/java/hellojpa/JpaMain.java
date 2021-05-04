1d9d4f21f79e7d207730c5fd254cdf3d22691f6c
Youngjin <youngjin033@gmail.com>
2021-05-04 17:14:30 +0900

    신규 스키마 생성 및 필드, 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -9,17 +9,20 @@ import java.util.List;
 public class JpaMain {
     public static void main(String[] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
+
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try{
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
+            em.persist(member);
             //실제론 스프링이 나머지 코드를 다 실행해주어 persist만 하면됨
             //엔티티 대상으로 쿼리를 날리게 코드 작성(DB, 방언에 종속적이지 않음)
-
-            Member member = em.find(Member.class, 150L);    //find로 찾은 것은 영속 상태이다
-            member.setName("ZZZZ");
-            
             //em.clear() 영속성 컨텍스트 완전히 초기화
             //em.persist(member1); //1차 캐시에 저장(버퍼링)
             //em.detach(member); 분리, 준영속 상태로 jpa가 관리하지 않는다(커밋해도 아무일 X) remove: 삭제
